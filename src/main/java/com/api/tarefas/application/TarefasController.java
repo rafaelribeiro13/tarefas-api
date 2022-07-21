17ba1e17d51e9ae1c1a49fd4b3226283b81ed471
Rafael Ribeiro <rafaelsribeiro45@gmail.com>
2022-07-21 20:16:24 -0300

    Criação dos métodos de busca e deleção  por ID

--- src/main/java/com/api/tarefas/application/TarefasController.java
+++ src/main/java/com/api/tarefas/application/TarefasController.java
@@ -1,17 +1,20 @@
-package com.api.tarefas.API.controllers;
+package com.api.tarefas.application;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.tarefas.dominio.Tarefa;
 import com.api.tarefas.infraestrutura.RepositorioTarefas;
 
-@RequestMapping("/tarefas")
+// @RequestMapping("/tarefas")
 @RestController
 public class TarefasController {
     
@@ -21,14 +24,29 @@ public class TarefasController {
         repositorioTarefas = new RepositorioTarefas();
     }
 
-    @GetMapping("/")
+    @GetMapping("/tarefas/{id}")
+    public Tarefa obterTarefaPeloId(@PathVariable int id) {
+        return repositorioTarefas.obterTarefaPorId(id);
+    }
+
+    @GetMapping("/tarefas")
     public List<Tarefa> obterTarefas() {
         return repositorioTarefas.getTarefas();
     }
 
-    @PostMapping("/")
+    @PostMapping("/tarefas")
     public List<Tarefa> criarTarefa(@RequestBody Tarefa tarefa) {
         return repositorioTarefas.adicionarTarefa(tarefa);
     }
     
+    // delete
+    @DeleteMapping("/tarefas/{id}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public void excluirTarefa(@PathVariable int id) {
+        repositorioTarefas.excluirTarefa(id);
+    }
+
+    // update
+
+
 }
--- src/main/java/com/api/tarefas/infraestrutura/RepositorioTarefas.java
+++ src/main/java/com/api/tarefas/infraestrutura/RepositorioTarefas.java
@@ -1,8 +1,8 @@
 package com.api.tarefas.infraestrutura;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Repository;
 
@@ -11,11 +11,33 @@ import com.api.tarefas.dominio.Tarefa;
 @Repository
 public class RepositorioTarefas {
     
-    private List<Tarefa> tarefas = new ArrayList<>();
+    private List<Tarefa> tarefas = List.of(
+        new Tarefa(20, "Estudar Spring"),
+        new Tarefa(78, "Estudar API"),
+        new Tarefa(178784, "Estudar Java")
+    );
 
     public List<Tarefa> adicionarTarefa(Tarefa tarefa) {
         tarefas.add(tarefa);
         return tarefas;
+    }
+
+    public void excluirTarefa(int id) {
+        Tarefa tarefa = obterTarefaPorId(id);
+        if (tarefa != null) {
+            tarefas.remove(tarefa);
+        }
+    }
+
+    public Tarefa obterTarefaPorId(int id) {
+        if (tarefas.size() > 0) {
+            return tarefas.stream()
+                .filter(x -> x.getId() == id)
+                .collect(Collectors.toList())
+                .get(0);
+        }
+
+        return null;
     }
 
     public List<Tarefa> getTarefas() {
